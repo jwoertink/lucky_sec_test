2e45c0e400ae308eb803af6d62ea7cda4cb856dd
Bar Hofesh <bar.hofesh@gmail.com>
2022-02-07 12:18:39 +0200

    try no CSRF

--- config/application.cr
+++ config/application.cr
@@ -9,3 +9,7 @@ Application.configure do |settings|
   settings.name = "Lucky SecTest"
   settings.support_email = Carbon::Address.new(settings.name, "no-reply@lucky_sectest.com")
 end
+
+Lucky::ProtectFromForgery.configure do |config|
+  config.allow_forgery_protection = false
+end
